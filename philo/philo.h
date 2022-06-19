a65e82d84e300e451b84fb47e05fa5151afcb172
Nastya880 <56202516+Nastya880@users.noreply.github.com>
2022-06-19 17:25:53 +0300

    add struct with input data

---
+++ philo/philo.h
@@ -0,0 +1,19 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct s_args
+{
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				time_must_eat;
+}				t_args;
+
+#endif
